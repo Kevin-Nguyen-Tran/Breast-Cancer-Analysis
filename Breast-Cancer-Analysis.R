06994b6fcf511f04ff4d0e8f0052a27c56c6354d
Kevin Tran <k.n.tran898@gmail.com>
2020-09-13 13:48:30 -0400

    Fixed bug for summarise and group_by. loaded the libraries in a particular order to mask over the Rmisc with tidyverse.

--- Breast-Cancer-Analysis.R
+++ Breast-Cancer-Analysis.R
@@ -4,10 +4,11 @@
 
 rm(list = ls()) #removes all variables stored previously in Environment (good habit to include)
 
-library(tidyverse) #Run tidyverse, if you do not have: install.packages("tidyverse")
+library(Rmisc) # transform integers to factors, must be first or will mask other packages. We only need multiplot!
 library(lubridate) #to modify date-time entries, if you do not have it: install.packages("lubridate")
 library(scales) #allows to modify scientific notation for values
-library(forcats) # transform integers to factors
+library(dplyr)
+library(tidyverse) #Run tidyverse, if you do not have: install.packages("tidyverse")
 
 # https://www.kaggle.com/uciml/breast-cancer-wisconsin-data
 
@@ -40,18 +41,24 @@ mean_bc_data <- bc_data %>%
 
 # We can do a multiple linear regression model for the radius, perimeter, and area to diagnosis.
 # let's start off with doing individual comparisons. radius -> perimeter -> area.
+
+#------------------------------------------------------------------------------------- BOX PLOTS
+
 # I hypothesize that with increasing averages, there is a higher likelihood of malignancy diagnosis: let's look at boxplots first
 
-ggplot(mean_bc_data, aes(x = diagnosis, y = radius_mean)) +
+radius_bp <- ggplot(mean_bc_data, aes(x = diagnosis, y = radius_mean)) +
   geom_boxplot()
 
-mean_bc_data %>%
+data %>%
   group_by(diagnosis) %>%
   summarise(mean(radius_mean)) # average radius Benign = 12.1 and Malignant = 17.5
 
+t.test(mean_bc_data$radius_mean ~ mean_bc_data$diagnosis, mu = 0, alt = "two.sided", conf = 0.95, var.eq = FALSE, paired = FALSE)
+
+
 #-------------------------------------------------------------------------------------
 
-ggplot(mean_bc_data, aes(x = diagnosis, y = perimeter_mean)) +
+peri_bp <- ggplot(mean_bc_data, aes(x = diagnosis, y = perimeter_mean)) +
   geom_boxplot()
 
 mean_bc_data %>%
@@ -60,7 +67,7 @@ mean_bc_data %>%
 
 #-------------------------------------------------------------------------------------
 
-ggplot(mean_bc_data, aes(x = diagnosis, y = area_mean)) +
+area_bp <- ggplot(mean_bc_data, aes(x = diagnosis, y = area_mean)) +
   geom_boxplot()
 
 mean_bc_data %>%
@@ -68,49 +75,41 @@ mean_bc_data %>%
   summarise(mean(area_mean)) # average radius Benign = 463 and Malignant = 978
 
 #For all three boxplots, they show a dramatic difference between average size. There is a positive correlation between radius, perimeter, and area to categorization of malignancy.
+multiplot(radius_bp, peri_bp, area_bp, cols = 2) #the above 3 plots plotted on a single graph for visibility purposes
 
-#-------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------- DENSITY PLOTS
+
+radius_dp <- ggplot(mean_bc_data, aes(x = radius_mean, fill = diagnosis)) +
+  geom_density(size = 1, alpha = .5)
+
+peri_dp <- ggplot(mean_bc_data, aes(x = perimeter_mean, fill = diagnosis)) +
+  geom_density(size = 1, alpha = .5)
+
+area_dp <- ggplot(mean_bc_data, aes(x = area_mean, fill = diagnosis)) +
+  geom_density(size = 1, alpha = .5)
+
+multiplot(radius_dp, peri_dp, area_dp, cols = 2) #the above 3 plots plotted on a single graph for visibility purposes
+
+# It is interesting to see that there is a shared relationship between proprotions of those with a partifuclar diagnosis and size of cell nuclei.
+# It is not surprising since a greater radius would signifiy a greater perimeter, which would signify a greater area.
+# A density plot is great to see how the patients are distributed by their diagnosis and to see how the peaks signify at a particular x value, where the most patients will fall into that bin.
+
+
+#------------------------------------------------------------------------------------- SCATTER PLOTS
 
 # what would it look like if we created a scatter plot between the average radius, perimeter and area (color to reflect diagnosis)
 # All sharing a positive correlation
 
-ggplot(mean_bc_data, aes(x = radius_mean, y = perimeter_mean, color = diagnosis)) +
+rp_sp <- ggplot(mean_bc_data, aes(x = radius_mean, y = perimeter_mean, color = diagnosis)) +
   geom_point()
 
-ggplot(mean_bc_data, aes(x = radius_mean, y = area_mean, color = diagnosis)) +
+ra_sp <- ggplot(mean_bc_data, aes(x = radius_mean, y = area_mean, color = diagnosis)) +
   geom_point()
 
-ggplot(mean_bc_data, aes(x = perimeter_mean, y = area_mean, color = diagnosis)) +
+pa_sp <- ggplot(mean_bc_data, aes(x = perimeter_mean, y = area_mean, color = diagnosis)) +
   geom_point()
 
-#------------------------------------------------------------------------------------
-
-# since we know that there is a linear relationship, let's see if all 3 are good predictors of malignancy
-# Logistic Linear Regression - Binomial
-
-predicted <- glm(diagnosis ~ radius_mean + perimeter_mean + area_mean, family = "binomial", data = mean_bc_data)
-summary(predicted)
-
-#Why is radius negative?!
-
-probability_data <- data.frame(fitted.values = predicted$fitted.values, status = mean_bc_data$diagnosis)
-
-probability_data <- probability_data %>%
-  arrange(fitted.values)
-
-probability_data <- probability_data %>%
-  mutate(rank = 1:nrow(probability_data))
-
-ggplot(probability_data, aes(x = rank, y = fitted.values, color = status)) +
-  geom_point(alpha = 1, shape = 1, stroke = 2)
-
-#As seen by the graph, our glm has accurately captured the relationship of the 3 variables to diagnosis
-# The 3 values are all good predictors of malignancy as shown by the small p-values
-
-t.test(mean_bc_data$radius_mean ~ mean_bc_data$diagnosis, mu = 0, alt = "two.sided", conf = 0.95, var.eq = FALSE, paired = FALSE)
-
-# Curious to see why the radius has a negative relationship to diagnosis. but the other 2 have positive relationships.
-# Will have to look into this a bit more*********************
+multiplot(rp_sp, ra_sp, pa_sp, cols = 2)
 
 
 #=============================================================================================================================================================================================================================
@@ -199,24 +198,54 @@ mean_bc_data %>%
 # With that being said, can we use number of concave points as an indicator for malignancy within a cell nuclei?
 # such as that > than .0853 is a good indicator of malignancy? Can this be applied to the population? Great questions!
 
+
 #=============================================================================================================================================================================================================================
-# SECTION 4: SYMMETRY AND FRACTAL DIMENSIONS TO CELL NUCLEI DIAGNOSIS (BENIGN VS MALIGNANT)
+# COMPARISON OF AVERAGE VARIABLE TO WORST (LARGEST) VARIABLE SEPARATED BY DIAGNOSIS XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 #=============================================================================================================================================================================================================================
 
-# symmetry is where there is a sense in balance of proportions. So on an axis, both halves are equal. 0 signifying no symmetry and closer to 1 as signifying symmetry
-# We will have to research fractal dimensions in regards to coastline measurements: http://fractalfoundation.org/OFC/OFC-10-4.html
+#=============================================================================================================================================================================================================================
+# SECTION 1: RADIUS, PERIMETER, AND AREA XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+#=============================================================================================================================================================================================================================
+
+# cc_data <- bc_data %>%
+  #select(id:radius_mean, perimeter_mean:concave.points_mean, radius_worst, perimeter_worst:concave.points_worst)
+
+# radius_cc <- ggplot(data = cc_data, mapping = aes(x = radius_mean, y = radius_worst, color = diagnosis)) +
+ # geom_point()
+
+# ggplot(cc_data, aes(x = diagnosis, y = radius_worst)) +
+  #geom_boxplot()
 
 
 
 
+#=============================================================================================================================================================================================================================
+# LOGISTIC REGRESSION - BINOMIAL - FOR ALL ANALYZED VARIABLES IN RELATION TO DIAGNOSIS
+#=============================================================================================================================================================================================================================
 
 
+# I don't feel that adding the worst in comparison to the average adds anymore too the analysis. Everything is just shifted upward.
+mean_bc_data$diagnosis <- relevel(mean_bc_data$diagnosis, ref = "B") # changed the reference to be M and finding good predictors for it
 
+predicted <- glm(diagnosis ~ radius_mean + perimeter_mean + area_mean + smoothness_mean + compactness_mean + concavity_mean + concave.points_mean, family = "binomial", data = mean_bc_data)
+summary(predicted)
 
+# as notated by the small p-values, out of all of the variables we analyzed, it seems that area_mean and concave.points_mean 
+# are the most useful predictors of diagnosis!
 
+probability_data <- data.frame(fitted.values = predicted$fitted.values, status = mean_bc_data$diagnosis)
 
+probability_data <- probability_data %>%
+  arrange(fitted.values)
 
+probability_data <- probability_data %>%
+  mutate(rank = 1:nrow(probability_data))
 
+ggplot(probability_data, aes(x = rank, y = fitted.values, color = status)) +
+  geom_point(alpha = 1, shape = 1, stroke = 2)
+
+#As seen by the graph, our glm has accurately captured the relationship of the 3 variables to diagnosis
+# The 3 values are all good predictors of malignancy as shown by the small p-values
 
 
 
